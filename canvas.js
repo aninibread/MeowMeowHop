70f1a241dc5d0894e9480aa6a93321e07981a271
Anni Wang <54481763+aninibread@users.noreply.github.com>
2024-01-02 19:29:27 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -1,37 +1,45 @@
-var kitty;
-var cucumber = [];
-var myScore;
-var highScore;
-localStorage.setItem("highscore",0);
-var gameOver;
-var restartBtn;
-var storedHighScore;
-var meow;
-var bigStars =[];
-var watermelon = [];
-var baseUrl = 'https://pub-f279f96d372841da89bd0f36dbd5e0a4.r2.dev/';
+var kitty, cucumber = [], myScore, highScore, gameOver = false, restartBtn;
+var bigStars = [], watermelon = [];
+var images = {};  // Store all images here
+var meow, storedHighScore;
+var lastAnimationTime = Date.now();
+var animationInterval = 100;
 
-function startGame() 
-{
-    kitty = new component(52,42,baseUrl+"cat.png", 70, 300-30,"image");
-    myScore = new component("16px", "Monospace", "black", 800, 40, "text");
-    bigStars.push (new component (900,150,baseUrl+"bigStars.png",-100,0,"image"));
-    highScore = new component("16px", "Monospace", "black", 700, 40, "text");
-    meow = new sound("meow.mp3");
+function loadImage(name, src) {
+    var img = new Image();
+    img.src = src;
+    images[name] = img;
+}
+
+function startGame() {
+    loadImage('cat', 'cat.png');
+    loadImage('catDead', 'catDead.png');
+    loadImage('cat2', 'cat2.png');
+    loadImage('catUp', 'catUp.png');
+    loadImage('bigStars', 'bigStars.png');
+    loadImage('button', 'button.png');
+    loadImage('banana', 'banana.png');
+    loadImage('watermelon', 'watermelon.png');
+
+    kitty = new component(52, 42, 'cat', 70, 270, 'image');
+    myScore = new component('16px', 'Monospace', 'black', 800, 40, 'text');
+    bigStars.push(new component(900, 150, 'bigStars', -100, 0, 'image'));
+    highScore = new component('16px', 'Monospace', 'black', 700, 40, 'text');
+    meow = new sound('meow.mp3');
     myGameArea.start();
 }
 
 var myGameArea = {
-    canvas : document.createElement("canvas"),
+    canvas : document.createElement('canvas'),
     start : function() {
         this.canvas.width = 900;
         this.canvas.height = 300;
-        this.context = this.canvas.getContext("2d");
+        this.context = this.canvas.getContext('2d');
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
         this.frameNo2 = 0;
         this.scoreNum = 0;
-        this.interval = setInterval(updateGameArea, 3.7);
+        this.interval = setInterval(updateGameArea, 10);
         window.addEventListener('keydown', function (e) {
             myGameArea.key = e.keyCode;
         })
@@ -39,8 +47,8 @@ var myGameArea = {
             myGameArea.key = false;
         })
     },
-    clear: function() {
-        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     stop : function() {
         if(myGameArea.key && myGameArea.key == 32){
@@ -50,37 +58,10 @@ var myGameArea = {
     }
 }
 
-// kitty, score, cucumber
-function everyinterval(n) {
-    if ((myGameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-}
-//big star, watermelon
-function everyinterval2(n) {
-    if ((myGameArea.frameNo2 / n) % 1 == 0) {return true;}
-    return false;
-}
-
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-      this.sound.play();
-    }
-    this.stop = function(){
-      this.sound.pause();
-    }
-}
-
-function component(width, height, color, x, y, type){
+function component(width, height, color, x, y, type) {
     this.type = type;
-    if(type == "image"){
-        this.image = new Image();
-        this.image.src = color;
+    if (type == 'image') {
+        this.image = images[color];
     }
     this.width = width;
     this.height = height;
@@ -88,21 +69,14 @@ function component(width, height, color, x, y, type){
     this.y = y;
     this.gravity = 0.05;
     this.gravitySpeed = 0;
-    this.update = function(){
+    this.update = function() {
         ctx = myGameArea.context;
-        if (this.type == "text") {
-            ctx.font = this.width + " " + this.height;
+        if (this.type == 'text') {
+            ctx.font = this.width + ' ' + this.height;
             ctx.fillStyle = color;
             ctx.fillText(this.text, this.x, this.y);
-        } else {
-            ctx.fillStyle = color;
-            ctx.fillRect(this.x, this.y, this.width, this.height);
-        }
-        if (type == "image"){
-            ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
+        } else if (type == 'image') {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         } else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
@@ -136,112 +110,116 @@ function component(width, height, color, x, y, type){
         var otherbottom = otherobj.y + (otherobj.height);
         var crash = true;
         if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-          crash = false;
+            crash = false;
         }
         return crash;
     }
 }
 
-function jumping () {
+function sound(src) {
+    this.sound = document.createElement('audio');
+    this.sound.src = src;
+    this.sound.setAttribute('preload', 'auto');
+    this.sound.setAttribute('controls', 'none');
+    this.sound.style.display = 'none';
+    document.body.appendChild(this.sound);
+    this.play = function(){
+        this.sound.play();
+    }
+    this.stop = function(){
+        this.sound.pause();
+    }
+}
+
+function jumping() {
     kitty.gravitySpeed = -3.3;
 }
 
-//cucumber
-min = 125;
-max = 500;
-gap = ranGap();
+var min = 125, max = 500, gap = ranGap();
 function ranGap() {
-    return Math.floor( min + Math.random() * ( max - min + 1 ));
+    return Math.floor(min + Math.random() * (max - min + 1));
 }
 
 function isEven(n) {
     return n % 2 == 0;
 }
 
-function checkHighScore (n) {
-    if (n > window.localStorage.getItem("highscore")) {
-        window.localStorage.setItem("highscore", JSON.stringify(n));
-      }
+function checkHighScore(n) {
+    if (n > window.localStorage.getItem('highscore')) {
+        window.localStorage.setItem('highscore', JSON.stringify(n));
+    }
 }
 
 function updateGameArea() {
     var x, y;
-
-    // cumcumber
     for (i = 0; i < cucumber.length; i += 1) {
         if (kitty.crashWith(cucumber[i])) {
-            
             checkHighScore(myGameArea.scoreNum);
-            highScore.text = "HI: " + parseInt(window.localStorage.getItem("highscore"));
+            highScore.text = 'HI: ' + parseInt(window.localStorage.getItem('highscore'));
             highScore.update();
-
-            meow.play();
-            gameOver = new component("35px", "Monospace", "black", 358, 120, "text");
-            restartBtn = new component(70,70,baseUrl+"button.png", 408, 140,"image");
-            gameOver.text = "GAME OVER";
-            kitty.image.src = baseUrl+"catDead.png";
+            gameOver = new component('35px', 'Monospace', 'black', 358, 120, 'text');
+            restartBtn = new component(70, 70, 'button', 408, 140, 'image');
+            gameOver.text = 'GAME OVER';
+            kitty.image = images['catDead'];
             kitty.update();
             gameOver.update();
             restartBtn.update();
             myGameArea.stop();
             return;
-            
         }
     }
-    
     myGameArea.frameNo2 += 1;
     myGameArea.frameNo += 1;
     myGameArea.scoreNum += 0.03;
     myGameArea.clear();
 
-    if (isEven(myGameArea.frameNo)){
-        kitty.image.src = baseUrl + "cat2.png";
-    } else {
-        kitty.image.src = baseUrl + "cat.png";
+    
+    var currentTime = Date.now();
+    if (currentTime - lastAnimationTime > animationInterval) {
+        lastAnimationTime = currentTime;
+        if (isEven(Math.floor(currentTime / animationInterval))) {
+            kitty.image = images['cat2'];
+        } else {
+            kitty.image = images['cat'];
+        }
     }
 
-    //cucumber
     if (everyinterval(gap)) {
         x = myGameArea.canvas.width;
         y = myGameArea.canvas.height - 50;
-        cucumber.push(new component(20, 50, baseUrl+"banana.png", x, y,"image"));
+        cucumber.push(new component(20, 50, 'banana', x, y, 'image'));
         gap = ranGap();
         myGameArea.frameNo = 0;
-        
     }
     for (i = 0; i < cucumber.length; i += 1) {
         cucumber[i].x += -1;
         cucumber[i].update();
     }
-    
-    //bigStars
+
     if (myGameArea.frameNo2 == 1 || everyinterval2(900)) {
         x = 900;
         y = 0;
-        bigStars.push (new component (900,150,baseUrl+"bigStars.png",x,y,"image"));
+        bigStars.push(new component(900, 150, 'bigStars', x, y, 'image'));
     }
     for (j = 0; j < bigStars.length; j += 1) {
         bigStars[j].x += -1;
         bigStars[j].update();
     }
 
-    //watermelon
-    if (everyinterval(3)) {
+    if (everyinterval(4)) {
         x = 3;
         y = kitty.y;
-        watermelon.push (new component (3,15, baseUrl+"watermelon.png", kitty.x +20, kitty.y + 13, "image"));
+        watermelon.push(new component(3, 15, 'watermelon', kitty.x + 20, kitty.y + 13, 'image'));
     }
+    
     for (g = 0; g < watermelon.length; g += 1) {
         watermelon[g].x += -1;
         watermelon[g].update();
     }
 
-    //score
-    myScore.text = "SCORE: " + myGameArea.scoreNum.toFixed(0);
+    myScore.text = 'SCORE: ' + myGameArea.scoreNum.toFixed(0);
     myScore.update();
-
-    //highscore
-    highScore.text = "HI: " + parseInt(window.localStorage.getItem("highscore"));
+    highScore.text = 'HI: ' + parseInt(window.localStorage.getItem('highscore'));
     highScore.update();
 
     //space upwards
@@ -261,9 +239,26 @@ function updateGameArea() {
 
     //cat tail down
     if(myGameArea.key) {
-        kitty.image.src = baseUrl+"catUp.png";
+        kitty.image = images['catUp'];
+    }
+
+    if (myGameArea.key && myGameArea.key == 40) {
+        kitty.gravitySpeed = 2.8;
     }
 
     kitty.newPos();
     kitty.update();
 }
+
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) return true;
+    return false;
+}
+
+function everyinterval2(n) {
+    if ((myGameArea.frameNo2 / n) % 1 == 0) return true;
+    return false;
+}
+
+// Initialize the game
+startGame();
